9acd12ac2679bb5a26b2fc7cb5680c4448445ec8
Wesly <ultimatecracker88@gmail.com>
2019-06-10 23:24:38 -0400

    Exercise ex2

---
+++ js/ex2.js
@@ -0,0 +1,12 @@
+// VARIABLES
+
+var pais = "PR";
+var contiente = "Latinoamerica";
+var antiguedad = 2019;
+var pais_y_contiente = pais + ' ' + contiente;
+
+pais = "Rusia"
+contiente = "Asia";
+
+console.log(pais, contiente, antiguedad);
+alert(pais_y_contiente);
