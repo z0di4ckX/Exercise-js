ec835f0f05f4aa8f7b7e912f0b590c5094a6f55f
Wesly <ultimatecracker88@gmail.com>
2019-06-11 22:48:36 -0400

    Exercise ex3

---
+++ js/ex3.js
@@ -0,0 +1,21 @@
+var numero = 40;
+console.log(numero);
+
+if(true) {
+    var numero = 50;
+    console.log(numero);
+}
+
+console.log(numero);
+
+// prueba con let
+
+var texto = "Curso JS";
+console.log(texto);
+
+if(true) {
+    let texto = "Curso Laravel 5";
+    console.log(texto);
+}
+
+console.log(texto);
