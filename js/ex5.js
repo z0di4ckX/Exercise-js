b35dd130277d93e0127b3e94ddba5c2bb69fd29b
Wesly <ultimatecracker88@gmail.com>
2019-06-18 12:41:16 -0400

    Exercise ex5

---
+++ js/ex5.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// Operadores
+
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 + numero2;
+
+alert("Es resultado de la operacion es: " + operacion);
+
+// tipos de datos
+
+var numeroEntero = 44;
+var cadenaDeTexto = "Hola que tal";
+var verdaderoOFalso = false;
+
+var numeroFalse = "33.2";
+
+console.log(String(numeroEntero) + 4 );
