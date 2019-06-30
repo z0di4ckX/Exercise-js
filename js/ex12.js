6e67693ccc01b5e37e407e08f866cd0533a22ccc
Wesly <ultimatecracker88@gmail.com>
2019-06-30 13:19:37 -0400

    Exercise ex12 alert type

---
+++ js/ex12.js
@@ -0,0 +1,16 @@
+'use strict'
+
+// Alerta
+
+alert("Esta es mi alert");
+alert("Este es mi texto");
+
+// Confirmacion
+
+var mi_resultado = confirm("Estas seguro de querer continuar?");
+console.log(mi_resultado);
+
+// Ingreso datos
+
+var mi_resultado2 = prompt("Que edad tienes? ", 18);
+console.log(typeof mi_resultado2);
