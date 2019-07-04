122c12885d4f8ede13bdca33077dc307f6c1ad77
Wesly <ultimatecracker88@gmail.com>
2019-07-04 10:46:25 -0400

    Exercise ex2

---
+++ project/ex2.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/**
+ * Utilizando un bucle, mostrar la suma y la media de los numeros introducidos
+ * hasta introducir un numero negativo y ahi mostrar el resultado
+ */
+
+ var suma = 0;
+ var contador = 0;
+
+ do {
+    var numero = parseInt(prompt('Introduce numero hasta que metas a uno negativo', 0));
+
+    if(isNaN(numero)) {
+        numero = 0;
+    }
+    else if(numero >= 0) {
+        suma += numero;
+
+        contador++;
+    }
+ }while(numero >= 0);
