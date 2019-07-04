9c5d07c6ea796ca6cc8792c0b2daf9e3d83c4601
Wesly <ultimatecracker88@gmail.com>
2019-07-04 10:33:06 -0400

    Plus in exercise 1

--- project/ex1.js
+++ project/ex1.js
@@ -1,11 +1,15 @@
 'use strict'
 
 // Programa que pida dos numeros y que nos diga cual es el mayor, el menor y si son iguales.
+// Plus: Si los numeros no son un numero o son menores o iguales a cero, nos los vuelva a pedir
 
 var numero1 = parseInt(prompt('Introduce el primer numero: ', 0));
 var numero2 = parseInt(prompt('Introduce el segundo numero: ', 0));
 
-console.log(numero1, numero2);
+while(numero1 <= 0 || numero2 <= 0 || isNaN(numero1) || isNaN(numero2)) {
+    numero1 = parseInt(prompt('Introduce el primer numero: ', 0));
+    numero2 = parseInt(prompt('Introduce el segundo numero: ', 0));    
+}
 
 if(numero1 == numero2) {
     alert("Los numero son iguales");
