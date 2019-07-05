20457969060fc4c1c3d30fbc743a9539bc3f9183
z0di4ckX <ultimatecracker88@gmail.com>
2019-07-05 15:02:54 -0400

    Exercise ex5

---
+++ project/ex5.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/**
+ * Muestre todos los numeros divisores de un numero introduce en prompt
+ */
+
+var numero  = parseInt(prompt("Introduce un numero: ", 1));
+
+for(var i = 1; i <= numero; i++) {
+    
+    if(numero % i == 0) {
+        document.write(`Divisor: ${i} <br/>`);
+    }
+}
