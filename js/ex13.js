dcb07dc270baceb59762d6b270dddb6ed81cfc5f
Wesly <ultimatecracker88@gmail.com>
2019-08-01 18:27:50 -0400

    Exercise 13 function

---
+++ js/ex13.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// Funciones
+
+// Una funcion es una agrupacion reutilizable de un conjunto de instrucciones
+
+// Defino la funcion
+function claculadora() {
+    
+    // Conjunto de instrucciones
+    console.log("Hola soy una calculadora");
+    
+    console.log("Si soy yo");
+
+    return "Hola soy la calculadora nuevamente!";
+}
+
+// Invocar o llamar a la funcion
+var resultado = claculadora();
+
+console.log(resultado);
