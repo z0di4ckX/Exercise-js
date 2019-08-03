7580bd4ccff9cf086f606b465bd35b85a0475add
Wesly <ultimatecracker88@gmail.com>
2019-08-03 14:53:57 -0400

    Exercise 20 ambitos

---
+++ js/ex20.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// ambito
+
+function holaMundo(texto) {
+    var hola_Mundo = "Texto dentro de una funcion";
+
+    console.log(numero.toString());
+    console.log(hola_Mundo);
+    console.log(texto);
+}
+
+var numero = 12;
+var texto = "Hola mundo desde una varibale global";
+holaMundo(texto);
