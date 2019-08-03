4e225c0bc0cb106be0d3ae2a50c3d84d2e17d9a4
Wesly <ultimatecracker88@gmail.com>
2019-08-02 23:36:44 -0400

    Exercise 15 function parametors opcionales

---
+++ js/ex15.js
@@ -0,0 +1,31 @@
+'use strict'
+
+// Funciones
+
+// Una funcion es una agrupacion reutilizable de un conjunto de instrucciones
+
+// Defino la funcion
+function claculadora(n1, n2, mostrar = false) {
+    
+    if(mostrar == false) {
+
+        // Conjunto de instrucciones
+        console.log("Suma: " + (n1 + n2));
+        console.log("Resta: " + (n1 - n2));
+        console.log("Multiplicacion: " + (n1 * n2));
+        console.log("Division: " +( n1 / n2));
+        console.log("***********************");
+    }
+    else {
+        
+        document.write("Suma: " + (n1 + n2));
+        document.write("Resta: " + (n1 - n2));
+        document.write("Multiplicacion: " + (n1 * n2));
+        document.write("Division: " +( n1 / n2));
+        document.write("***********************");
+    }
+}
+
+// Invocar o llamar a la funcion
+claculadora(1, 4);
+claculadora(2, 5, true);
