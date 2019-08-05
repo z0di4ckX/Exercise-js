b860c0b8782201d6455b5cfda8cc1c28df1fa15d
Wesly <ultimatecracker88@gmail.com>
2019-08-05 19:09:47 -0400

    Excersie 24 array

---
+++ js/ex24.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// Arrays, Arreglos, Matrices
+
+var nombre = "Juan del Pueblo";
+var nombre = ["Juan del Pueblo", "Junito del Campo", "Manolo Garcia", "Jose Martin", 43, true];
+
+var lenguales = new Array("PhP", "JS", "GO", "Java","Python");
+
+console.log(nombre[2]);
