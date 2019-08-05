cdd94475fc4c3e41622ab4b1b67603c9b0794917
Wesly <ultimatecracker88@gmail.com>
2019-08-05 19:29:19 -0400

    Excersie 25 search array

---
+++ js/ex25.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// Arrays, Arreglos, Matrices
+
+var nombre = "Juan del Pueblo";
+var nombre = ["Juan del Pueblo", "Junito del Campo", "Manolo Garcia", "Jose Martin", 43, true];
+
+var lenguales = new Array("PhP", "JS", "GO", "Java","Python");
+
+// var elemento = parseInt(prompt("Que elemento del array quieres? ", 0));
+
+// if(elemento >= nombre.length) {
+//     alert(`Introduce el numero correcto menor que ${nombre.length}`);
+// }
+// else {
+    
+//     alert(`El usuario seleccionado es ${nombre[elemento]}`);
+// }
+
+
+document.write("<h1>Lenguajes de programacion del 2018</h1>");
+document.write("<ul>");
+
+for(var i = 0; i < lenguales.length; i++) {
+    
+    document.write("<li>" + lenguales[i] + "</li>");
+}
+
+document.write("</ul>");
