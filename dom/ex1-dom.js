fd34a5288c862cba4e618b7b21e3d0a4028b7f1c
Wesly <ultimatecracker88@gmail.com>
2019-08-07 13:47:49 -0400

    Exercise ex1 DOM

--- dom/ex1-dom.js
+++ dom/ex1-dom.js
@@ -6,11 +6,14 @@ function cambiaColor(color) {
     cajas.style.background = color;
 }
 
-var cajas = document.getElementById("miCajas");
+// var cajas = document.getElementById("miCajas");
+var cajas = document.querySelector("#miCajas");
+
 cajas.innerHTML = "! TEXTO DESDE JS !";
 
 cajas.style.background = "red";
 cajas.style.padding = "20px";
 cajas.style.color = "white";
+cajas.className = "hola";
 
 console.log(cajas);
