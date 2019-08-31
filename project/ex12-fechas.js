019fd927790bd8cdda70507bd9ca40445d79a6c3
Wesly <ultimatecracker88@gmail.com>
2019-08-31 16:13:41 -0400

    Exercise date objects

---
+++ project/ex12-fechas.js
@@ -0,0 +1,13 @@
+var fecha = new Date();
+
+var year = fecha.getFullYear();
+var mes = fecha.getMonth();
+var dia = fecha.getDate();
+var hora = fecha.getHours();
+
+var textHoras = `The year is: ${year}
+                 The month is: ${mes}
+                 The date is: ${dia}
+                 The hours is: ${hora}`;
+
+console.log(textHoras);
