b490e3876b15d191becc60bbd238e29f4241b79f
Wesly <ultimatecracker88@gmail.com>
2019-09-08 23:21:01 -0400

    Excerse 01-selectores

---
+++ JQUERY/js/01-selectores.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // Selector de ID
+
+    $("#rojo").css("background", "red")
+               .css("color", "white");
+    
+    $("#amarillo").css("background", "yellow")
+                  .css("color", "green");
+    
+    $("#verde").css("background", "green")
+                  .css("color", "yellow");
+});
