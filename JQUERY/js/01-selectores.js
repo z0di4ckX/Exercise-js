dbfcfc9e24f58cea1164cd52e5e847198936ca2a
Wesly <ultimatecracker88@gmail.com>
2019-09-10 23:04:22 -0400

    Exercise added class selector

--- JQUERY/js/01-selectores.js
+++ JQUERY/js/01-selectores.js
@@ -9,4 +9,10 @@ $(document).ready(function() {
     
     $("#verde").css("background", "green")
                   .css("color", "yellow");
+    
+    var miClase = $(".zebra").css("padding", "5px");
+
+    $(".sinBorde").click(function() {
+        $(this).addClass('zebra');
+    });
 });
